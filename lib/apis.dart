fc03a43c65ea4a3ab8088f9a1c280bb0766bf5f2
user <hmaidraza.engr@gmail.com>
2024-07-13 15:22:02 +0500

    metaxpertsdynamicapis

--- lib/apis.dart
+++ lib/apis.dart
@@ -1,3 +1,3 @@
 library metaxperts_dynamic_apis;
 String accountApi= "http://103.149.32.30:8080/ords/metaxperts/accounts/get/";
-String newaccounttimeApi = "http://103.149.32.30:8080/ords/metaxperts/newaccounttime/get/";
+String newaccounttimeApi = "http://10.149.32.30:8080/ords/metaxperts/newaccounttime/get/";
