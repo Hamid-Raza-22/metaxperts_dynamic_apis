1b710e019ebae071d1b9d2082eefe368d776a059
user <hmaidraza.engr@gmail.com>
2024-07-15 11:30:07 +0500

    metaxpertsdynamicapis

--- lib/apis.dart
+++ lib/apis.dart
@@ -2,14 +2,38 @@ library metaxperts_dynamic_apis;
 
 String loginApi = "http://103.149.32.30:8080/ords/metaxperts/login1/get/";
 String accountApi= "http://103.149.32.30:8080/ords/metaxperts/accounts/get/";
-String dummyAccountApi= "http://103.149.32.30:8080/ords/metaxperts/accounts/get/";
-String newAccountTimeApi = "http://10.149.32.30:8080/ords/metaxperts/newaccounttime/get/";
 String balance = "http://103.149.32.30:8080/ords/metaxperts/allbalance/get/";
 String recoveryForm = "http://103.149.32.30:8080/ords/metaxperts/recovery1/get/";
-String city ="https://apex.oracle.com/pls/apex/metaxpertss/city/get/";
+String city ="http://103.149.32.30:8080/ords/metaxperts/city/get/";
 String orderDetails= "http://103.149.32.30:8080/ords/metaxperts/detailsget/get/";
 String orderMaster = "http://103.149.32.30:8080/ords/metaxperts/masterget1/get/";
 String productsApi = "http://103.149.32.30:8080/ords/metaxperts/product1/get/";
 String shopDetails = "http://103.149.32.30:8080/ords/metaxperts/shopp1/get/";
 String orderBookingStatus = "http://103.149.32.30:8080/ords/metaxperts/statusget1/get/";
 String brandsApi ="http://103.149.32.30:8080/ords/metaxperts/brand1/get/";
+
+// Alternative Server APIs for GET
+String altLoginApi = "http://103.149.32.30:8080/ords/metaxperts/login1/get/";
+String altAccountApi= "http://103.149.32.30:8080/ords/metaxperts/accounts/get/";
+String altBalance = "http://103.149.32.30:8080/ords/metaxperts/allbalance/get/";
+String altRecoveryForm = "http://103.149.32.30:8080/ords/metaxperts/recovery1/get/";
+String altCity ="https://apex.oracle.com/pls/apex/metaxpertss/city/get/";
+String altOrderDetails= "http://103.149.32.30:8080/ords/metaxperts/detailsget/get/";
+String altOrderMaster = "http://103.149.32.30:8080/ords/metaxperts/masterget1/get/";
+String altProductsApi = "http://103.149.32.30:8080/ords/metaxperts/product1/get/";
+String altShopDetails = "http://103.149.32.30:8080/ords/metaxperts/shopp1/get/";
+String altOrderBookingStatus = "http://103.149.32.30:8080/ords/metaxperts/statusget1/get/";
+String altBrandsApi ="http://103.149.32.30:8080/ords/metaxperts/brand1/get/";
+
+// Refresh Server APIs for GET
+String refLoginApi = "http://103.149.32.30:8080/ords/metaxperts/logindata/get/";
+String refAccountApi= "http://103.149.32.30:8080/ords/metaxperts/newaccounttime/get/";
+String refBalance = "http://103.149.32.30:8080/ords/metaxperts/totalbalance/get/";
+String refRecoveryForm = "http://103.149.32.30:8080/ords/metaxperts/newaccounttime/get/";
+String refCity ="http://103.149.32.30:8080/ords/metaxperts/newcitties/get/";
+String refOrderDetails= "http://103.149.32.30:8080/ords/metaxperts/newdetailsgettime/get/";
+String refOrderMaster = "http://103.149.32.30:8080/ords/metaxperts/newmastergettime/get/";
+String refProductsApi = "http://103.149.32.30:8080/ords/metaxperts/newproductget/get/";
+String refShopDetails = "http://103.149.32.30:8080/ords/metaxperts/newshopp1/get/";
+String refOrderBookingStatus = "http://103.149.32.30:8080/ords/metaxperts/newstatusgettime/get/";
+String refBrandsApi ="http://103.149.32.30:8080/ords/metaxperts/newbrands/get/";
