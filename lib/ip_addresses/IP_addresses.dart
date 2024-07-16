d7864993c73bf2c6fb067a480d7cba2888d2dc38
Hamid Raza <hamidraza.engr@gmail.com>
2024-07-16 14:32:39 +0500

    ip addresses

--- lib/ip_addresses/IP_addresses.dart
+++ lib/ip_addresses/IP_addresses.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
- String IP_Address="";
+ String IP_Address="http://103.149.32.30:8080/ords/metaxperts";
  // String IP_Address2= "https://apex.oracle.com/pls/apex/metaxpertss";
 
 
